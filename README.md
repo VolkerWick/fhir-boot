5dbb2d64ebf6bd64eb9150fc43405f30107d764d
VolkerWick <volker.wick+github@gmail.com>
2020-05-30 19:14:54 +0200

    Adding spring-boot project

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 Demo project to implement FHIR Server on spring-boot.
 
 Step 1: empty repo.
+Step 2: adding empty spring-boot project & index.html, changing application.properties to ~.yml
+
 
 
---
+++ src/test/java/io/github/volkerwick/fhirboot/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.volkerwick.fhirboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
